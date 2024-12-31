884be309039362a33cddc8585348078cd4e4bf7d
AnsaAli <chinjuansa@example.com>
2024-12-31 13:41:49 +0530

    add heropage and netflix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import netFlixLogo from '../images/Netflix_Logo_PMS.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute px-10 py-10 bg-gradient-to-b from-black w-full' >
+        <img src={netFlixLogo} alt='netflixLogo' className='w-44 '/>
+    </div>
   )
 }
 
